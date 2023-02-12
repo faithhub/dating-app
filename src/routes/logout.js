4c4b0484afac08d3eb8fefcce696662149785bf8
faithhub <adebayooluwadara@gmail.com>
2023-02-12 16:51:02 +0100

    Update from User update profile

--- src/routes/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../middlewares/auth.middleware");
-const authController = require("../controllers/Auth/auth.controller");
-
-const router = express.Router();
-
-router.get("/", authController.logout);
-
-module.exports = router;
