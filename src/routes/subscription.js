2afefe2d3c9b8b5907e0df5109901d48e509ba3e
faithhub <adebayooluwadara@gmail.com>
2023-03-12 00:49:05 +0100

    Update from Post upload

--- src/routes/subscription.js
+++
@@ -1,9 +0,0 @@
-const api = require("../middlewares/API/api");
-const auth = require("../middlewares/API/auth");
-const subController = require("../controllers/API/subscription");
-const express = require("express");
-const router = express.Router();
-
-router.route("/").get(auth, subController.allSubs);
-
-module.exports = router;
