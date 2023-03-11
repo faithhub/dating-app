c14e6db18dc13cbaa793241931e16a3ac9eef02d
faithhub <adebayooluwadara@gmail.com>
2023-03-11 19:53:17 +0100

    Update from Post upload

--- src/controllers/API/post.js
+++ src/controllers/API/post.js
@@ -285,12 +285,12 @@ async function create(req, res) {
     console.log(req.body);
     const fullUrl = req.headers.host;
 
-    // return res.status(200).json({
-    //   message: "Post created successfully",
-    //   data: req.file,
-    //   fullUrl: fullUrl,
-    //   body: req.body,
-    // });
+    return res.status(200).json({
+      message: "Post created successfully",
+      data: req.file,
+      fullUrl: fullUrl,
+      body: req.body,
+    });
     if (req.file == undefined) {
       return res.status(400).send({ message: "Please upload the Post Image!" });
     }
