9513526c6b8469213ea1b5c3175d49ec91d71f45
faithhub <adebayooluwadara@gmail.com>
2023-03-12 02:06:37 +0100

    Update from Post and Avatar upload

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ const dotenv = require("dotenv");
 const flash = require("express-flash");
 const routes = require("./src/routes");
 
-log4js.configure({
-  appenders: { everything: { type: "file", filename: "logs.log" } },
-  categories: { default: { appenders: ["everything"], level: "ALL" } },
-});
-
-const logger = log4js.getLogger();
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -37,8 +31,8 @@ app.use(flash());
 
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //Auth
 const session = {
@@ -79,7 +73,6 @@ app.use("/", routes);
 //   });
 // });
 
-logger.debug("log message");
 app.use((req, res, next) => {
   const error = new Error("Page not found");
   error.status = 404;
